cf8b3c745911156fba8454e902ebd9562da82b8b
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2022-08-30 21:45:48 +0300

    handle request errors

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,11 +6,15 @@ import MealItem from "./MealItem/MealItem";
 const AvailableMeals = () => {
     const [meals, setMeals] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
 
     useEffect(() => {
         const fetchMeals = async () => {
             setIsLoading(true);
             const response = await fetch('https://react-http-25001-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+            if (!response.ok) {
+                throw new Error("Something went wrong...");
+            }
             const responseData = await response.json();
             const loadedMeals = [];
             for (const key in responseData) {
@@ -24,13 +28,24 @@ const AvailableMeals = () => {
             setMeals(loadedMeals);
             setIsLoading(false);
         }
-        return fetchMeals();
+        fetchMeals().catch(error => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        });
     }, [])
 
     if (isLoading) {
         return (
             <section className={classes.mealsLoading}>
                 <p>Loading...</p>
+            </section>
+        )
+    }
+
+    if (httpError) {
+        return (
+            <section className={classes.mealsError}>
+                <p>{httpError}</p>
             </section>
         )
     }
